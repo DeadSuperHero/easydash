474b20d8e2c728261507f06a075e687a8f8ce8e9
DeadSuperHero <sean@deadsuperhero.com>
2013-07-16 16:10:10 -0500

    tweak javascript for persona

--- app/assets/javascripts/persona_login.js
+++ app/assets/javascripts/persona_login.js
@@ -1,20 +1,33 @@
+/*
+* Our BrowserID login call. If the assertion was found, send this to Devise
+* with warden-browserid enabled for verification and login.
+*/
+function personaLogin(url) {
 $(".persona-button").click(function(e) {
   e.preventDefault();
 
   navigator.id.get(function(assertion) {
     if (assertion) {
+      // This code will be invoked once the user has successfully
+      // selected an email address they control to sign in with.
       $.ajax({
         url: '/users/sign_in',
         type: "POST",
-        dataType: "json",
-        cache: false,
-        data: {
-          "assertion": assertion
-        },
-        success: function(data, status) {
+        data: {"assertion": assertion},
+        dataTYpe: "json",
+        cache:false,
+        success:function(data,status){
+          console.log(data);
           window.location.href = '/';
         },
-      });
+        error:function(data,status){
+          alert(data.statusText + ": " + data.responseText);
+        }
+      })
+    } else {
+      // something went wrong! the user isn't logged in.
+      alert("Could not log you in!");
     }
+  })
   });
-});
+}
