f534b87615ca77c7ddabfb24d09fab6d13997101
DeadSuperHero <sean@deadsuperhero.com>
2013-07-20 07:53:44 -0500

    cleanup to layout, added navbar, persona button turns into user libravatar/gravatar and email string on successful login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-	def avatar_for(user, options = { size:50})
+	def avatar_for(user, options = {size: 40})
 		avatar_id = Digest::MD5::hexdigest(current_user.email).downcase
-		"https://seccdn.libravatar.org/avatar/#{avatar_id}.png"
+		"https://seccdn.libravatar.org/avatar/#{avatar_id}.png?s=40"
 	end
 end
