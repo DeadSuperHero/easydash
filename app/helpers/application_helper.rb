712dfe1bb929131c528e92766ff07a7ba6f6647d
DeadSuperHero <sean@deadsuperhero.com>
2013-07-20 06:29:31 -0500

    Add support for both Libravatar and Gravatar!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+	def avatar_for(user, options = { size:50})
+		avatar_id = Digest::MD5::hexdigest(current_user.email).downcase
+		"https://seccdn.libravatar.org/avatar/#{avatar_id}.png"
+	end
 end
